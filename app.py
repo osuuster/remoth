bb673ef953ec0099e8419d406ccd2c3f2eb4bb8b
Oliver Suuster <o.suuster@keelvar.com>
2022-11-23 20:44:30 +0000

    Add OSX support

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys
 from os import path
-from socket import gethostname, gethostbyname_ex, getfqdn
 from threading import Thread
 
 from flask import Flask
@@ -8,9 +7,21 @@ from keyboard import press
 from PIL import Image
 from pystray import Icon, Menu, MenuItem
 
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    platform = "linux"
+elif sys.platform == "darwin":
+    from osascript import osascript
+    platform = "OSX"
+elif sys.platform == "win32":
+    platform = "windows"
+else:
+    sys.exit()
+
 base_dir = path.join(sys._MEIPASS) if hasattr(sys, '_MEIPASS') else "."
-staticDir = path.join(base_dir, 'static')
-image = Image.open(staticDir + "\icon.ico")
+static_dir = path.join(base_dir, 'static')
+image_dir = path.join(static_dir, "icon.ico")
+image = Image.open(image_dir)
 
 icon = Icon('Remoth', icon=image,
             menu=Menu(MenuItem('Close', lambda x: x.stop())))
@@ -22,16 +33,19 @@ server.setDaemon(True) # Kills the server when icon is stopped
 
 @app.route("/press/<key>")
 def press_key(key):
+    if platform == "OSX" and key in ["volume_up", "volume_down"]:
+        # Temporary hack as volume controls are not supported for mac by keyboard library
+        inc = 2 if key == "volume_up" else -2
+        current_volume = int(osascript('get volume settings')[1].split(',')[0].split(':')[1])
+        osascript(f"set volume output volume {current_volume + inc}")
+        return f"{key} pressed"
     press(key)
     return f"{key} pressed"
 
 
 @app.route("/")
 def index():
-    return {
-        "ip": gethostbyname_ex(getfqdn())[2][1],
-        "host_name" : gethostname()
-    }
+    return "ok"
 
 
 if __name__ == "__main__":
